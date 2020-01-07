16eed28856013fdffacf20bec4eea9ababc61446
Sébastien LAURENT <sebastien.laurent@atos.net>
2020-01-07 11:37:37 +0100

    Initial plugin structure

---
+++ src/main/java/alien4cloud/plugin/k8s/spark/jobs/PluginConfiguration.java
@@ -0,0 +1,9 @@
+package alien4cloud.plugin.k8s.spark.jobs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "alien4cloud.plugin.k8s.spark.jobs")
+public class PluginConfiguration {
+}
