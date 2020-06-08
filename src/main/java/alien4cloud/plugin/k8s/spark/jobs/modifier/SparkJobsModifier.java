6e09815d68c7b10488267e35f11451c4726eb289
Sébastien LAURENT <sebastien.laurent@atos.net>
2020-06-08 15:51:40 +0200

    [ALIEN-3461] Simplification done by orchestrator plugin

--- src/main/java/alien4cloud/plugin/k8s/spark/jobs/modifier/SparkJobsModifier.java
+++ src/main/java/alien4cloud/plugin/k8s/spark/jobs/modifier/SparkJobsModifier.java
@@ -99,30 +99,7 @@ public class SparkJobsModifier extends TopologyModifierSupport {
     public void process(Topology topology, FlowExecutionContext context) {
         try {
             WorkflowValidator.disableValidationThreadLocal.set(true);
-
-            boolean updated = doProcess(topology, context);
-
-            TopologyContext topologyContext = workflowBuilderService.buildCachedTopologyContext(new TopologyContext() {
-                @Override
-                public String getDSLVersion() {
-                    return ToscaParser.LATEST_DSL;
-                }
-
-                @Override
-                public Topology getTopology() {
-                    return topology;
-                }
-
-                @Override
-                public <T extends AbstractToscaType> T findElement(Class<T> clazz, String elementId) {
-                    return ToscaContext.get(clazz, elementId);
-                }
-            });
-
-            if (updated) {
-                workflowSimplifyService.simplifyWorkflow(topologyContext, topology.getWorkflows().keySet());
-            }
-
+            doProcess(topology, context);
         } catch (Exception e) {
             log.warn("Can't process k8s-spark-jobs modifier:", e);
         } finally {
