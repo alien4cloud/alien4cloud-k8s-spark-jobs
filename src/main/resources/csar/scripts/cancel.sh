30b9c57ca40b0955d7159f3a92a6fc38c9b4b8b4
Emmanuel Duru <emmanuel.duru@atos.net>
2021-03-15 17:47:51 +0100

    [ALIEN-3009] job cancel implementation

--- src/main/resources/csar/scripts/cancel.sh
+++ src/main/resources/csar/scripts/cancel.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+getPodNameCmd="kubectl --kubeconfig ${KUBECONFIG_FILE} -n ${NAMESPACE} get po -l job_id=${TOSCA_JOB_ID} --no-headers -o custom-columns=:.metadata.name"
+if [ "$debug_operations" == "true" ]; then
+  echo "Get pod name command : $getPodNameCmd"
+fi
+
+POD_NAME=$(eval $getPodNameCmd)
+if [ "$debug_operations" == "true" ]; then
+  echo "pod name is : $POD_NAME"
+fi
+
+cleanPodCmd="kubectl --kubeconfig ${KUBECONFIG_FILE} -n ${NAMESPACE} delete po ${POD_NAME}"
+if [ "$debug_operations" == "true" ]; then
+  echo "Clean pod command : $cleanPodCmd"
+fi
+eval "$cleanPodCmd"
